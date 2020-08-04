9c89c2adfda8ef226a6f783c82578d69e10536b1
KanarekLife <kanareklife@gmail.com>
2020-08-04 16:50:30 +0200

    Add stopwatch (implementation of IStopwatch) tests

---
+++ tests/eru.Infrastructure.Tests/Stopwatch/StopwatchTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace eru.Infrastructure.Tests.Stopwatch
+{
+    public class StopwatchTests
+    {
+        [Fact]
+        public void DoesElapsedMillisecondsWorkCorrectly()
+        {
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+            stopwatch.Stop();
+            var implementation = new Infrastructure.Stopwatch.Stopwatch(stopwatch);
+
+            implementation.ElapsedMilliseconds.Should().Be(stopwatch.ElapsedMilliseconds);
+        }
+
+        [Fact]
+        public void DoesStopWorks()
+        {
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+            stopwatch.Stop();
+            var implementation = new Infrastructure.Stopwatch.Stopwatch(stopwatch);
+            implementation.Stop();
+            stopwatch.IsRunning.Should().BeFalse();
+        }
+
+        [Fact]
+        public void DoesStartWorks()
+        {
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            var implementation = new Infrastructure.Stopwatch.Stopwatch(stopwatch);
+            implementation.Start();
+            stopwatch.IsRunning.Should().BeTrue();
+        }
+    }
+}
