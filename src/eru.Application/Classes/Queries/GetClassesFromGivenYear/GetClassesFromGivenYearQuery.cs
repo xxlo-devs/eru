9efd4f4d562770e74f7ad53d981b7ad30bdf1518
Błażej Sak <bazik33@hotmail.com>
2020-08-15 16:18:01 +0200

    Added GetClassesFromGivenYear query

---
+++ src/eru.Application/Classes/Queries/GetClassesFromGivenYear/GetClassesFromGivenYearQuery.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using eru.Application.Classes.Queries.GetClasses;
+using eru.Application.Common.Interfaces;
+using eru.Domain.Enums;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace eru.Application.Classes.Queries.GetClassesFromGivenYear
+{
+    public class GetClassesFromGivenYearQuery : IRequest<IEnumerable<ClassDto>>
+    {
+        public Year Year { get; set; }
+    }
+
+    public class GetClassesFromGivenYearQueryHandler : IRequestHandler<GetClassesFromGivenYearQuery, IEnumerable<ClassDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetClassesFromGivenYearQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ClassDto>> Handle(GetClassesFromGivenYearQuery request, CancellationToken cancellationToken) 
+            => await _context.Classes
+                .Where(x => x.Year == request.Year)
+                .OrderBy(x => x.Name)
+                .ProjectTo<ClassDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+    }
+}
---
+++ tests/eru.Application.Tests/Classes/Queries/GetClassesFromGivenYearQueryTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using eru.Application.Classes.Queries.GetClasses;
+using eru.Application.Classes.Queries.GetClassesFromGivenYear;
+using eru.Application.Common.Mappings;
+using eru.Domain.Enums;
+using FluentAssertions;
+using Xunit;
+
+namespace eru.Application.Tests.Classes.Queries
+{
+    public class GetClassesFromGivenYearQueryTests
+    {
+        [Fact]
+        public async Task ShouldReturnAllClassesCorrectly()
+        {
+            var context =  new FakeDbContext();
+            var mapper = new Mapper(new MapperConfiguration(configuration =>
+            {
+                configuration.AddProfile<MappingProfile>();
+            }));
+            var handler = new GetClassesFromGivenYearQueryHandler(context, mapper);
+            var request = new GetClassesFromGivenYearQuery
+            {
+                Year = Year.Sophomore
+            };
+
+            var expectedClasses = new List<ClassDto>()
+            {
+                new ClassDto {Name = "II b", Year = Year.Sophomore}
+            };
+
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            result.Should().HaveCount(1).And.BeEquivalentTo(expectedClasses);
+        }
+    }
+}
