c2b35059d5ffbf298b4a64e4433cafcdaaae4eb9
KanarekLife <kanareklife@gmail.com>
2020-08-17 22:26:48 +0200

    Add first version of admin dashboard. Mostly design.

---
+++ src/eru.WebApp/Areas/AdminDashboard/Controllers/AdminDashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eru.WebApp.Areas.AdminDashboard.Controllers
+{
+    [Route("admin")]
+    [Area("AdminDashboard")]
+    public class AdminDashboardController : Controller
+    {
+        public IActionResult Dashboard()
+            => View();
+    }
+}
