ae8175b4ba0dd4b72235f2e3b326667c6abe8daf
KanarekLife <kanareklife@gmail.com>
2020-08-18 12:08:09 +0200

    Add seq to handle logs.

--- src/eru.WebApp/Program.cs
+++ src/eru.WebApp/Program.cs
@@ -1,7 +1,4 @@
-using System;
-using System.IO;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
@@ -9,44 +6,19 @@ namespace eru.WebApp
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(GetConfiguration())
-                .CreateLogger();
-
-            try
-            {
-                Log.Information("Starting up");
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Application start-up failed");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
+        public static void Main(string[] args) => 
+            CreateHostBuilder(args).Build().Run();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog()
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-        
-        /// <summary>
-        /// Makes configuration for serilog (Environment Variable [For docker] => Json file [For normal hosting])
-        /// </summary>
-        /// <returns></returns>
-        private static IConfiguration GetConfiguration()
-        {
-            var builder = new HostBuilder();
-            builder.UseContentRoot(Directory.GetCurrentDirectory());
-            var config = new ConfigurationBuilder()
-                .AddEnvironmentVariables("DOTNET_")
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-            return config.Build();
-        }
+                .UseSerilog((hostBuilderContext, serviceProvider, loggerConfiguration) =>
+                {
+                    loggerConfiguration
+                        .ReadFrom.Configuration(hostBuilderContext.Configuration);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
