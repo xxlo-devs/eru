98575bd9001e44ec2bc484a84a02d47a682f1fed
Blazej Sak <bazik33@hotmail.com>
2020-08-25 21:15:19 +0200

    adds reference ClassesParser implementation

---
+++ src/eru.Infrastructure/ClassesParser/ClassesParser.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using eru.Application.Common.Interfaces;
+using eru.Domain.Entity;
+
+namespace eru.Infrastructure.ClassesParser
+{
+    public class ClassesParser : IClassesParser
+    {
+        public Task<Class> Parse(string name)
+        {
+            name = name.Trim();
+
+            if (Regex.IsMatch(name[0].ToString(), "[0-9]"))
+            {
+                if (Regex.IsMatch(name[1].ToString(), "[0-9]"))
+                {
+                    var number = int.Parse(name.Substring(0, 2));
+                    if (number == 10 || number == 11 || number == 12)
+                        return Task.FromResult(new Class(number, name.Substring(2)));
+                }
+
+                return Task.FromResult(new Class(int.Parse(name.Substring(0, 1)), name.Substring(1)));
+            }
+
+            if (name.StartsWith("I"))
+            {
+                if (name.StartsWith("II"))
+                {
+                    if (name.StartsWith("III"))
+                    {
+                        return Task.FromResult(new Class(3, name.Substring(3)));
+                    }
+
+                    return Task.FromResult(new Class(2, name.Substring(2)));
+                }
+
+                if (name.StartsWith("IV"))
+                {
+                    return Task.FromResult(new Class(4, name.Substring(2)));
+                }
+
+                if (name.StartsWith("IX"))
+                {
+                    return Task.FromResult(new Class(9, name.Substring(2)));
+                }
+
+                return Task.FromResult(new Class(1, name.Substring(1)));
+            }
+
+            if (name.StartsWith("V"))
+            {
+                if (name.StartsWith("VI"))
+                {
+                    if (name.StartsWith("VII"))
+                    {
+                        if (name.StartsWith("VIII"))
+                        {
+                            return Task.FromResult(new Class(8, name.Substring(4)));
+                        }
+
+                        return Task.FromResult(new Class(7, name.Substring(3)));
+                    }
+
+                    return Task.FromResult(new Class(6, name.Substring(2)));
+                }
+
+                return Task.FromResult(new Class(5, name.Substring(1)));
+            }
+
+            if (name.StartsWith("X"))
+            {
+                if (name.StartsWith("XI"))
+                {
+                    if (name.StartsWith("XII"))
+                    {
+                        return Task.FromResult(new Class(12, name.Substring(3)));
+                    }
+
+                    return Task.FromResult(new Class(11, name.Substring(2)));
+                }
+
+                return Task.FromResult(new Class(10, name.Substring(1)));
+            }
+
+            throw new Exception("Class must have a year number!");
+        }
+
+        public Task<IEnumerable<Class>> Parse(IEnumerable<string> names)
+        {
+            var classes = names.ToArray().Select(className => Parse(className).Result).ToList();
+
+            return Task.FromResult(classes.AsEnumerable());
+        }
+    }
+}
---
+++ tests/eru.Infrastructure.Tests/ClassesParser/ClassesParserTests.cs
@@ -0,0 +1,88 @@
+﻿using eru.Domain.Entity;
+using FluentAssertions;
+using Xunit;
+
+namespace eru.Infrastructure.Tests.ClassesParser
+{
+    public class ClassesParserTests
+    {
+        [Fact]
+        public async void CanParseEveryClassFormat()
+        {
+            var classNames = new[] {"1a", "2A", "3 a", "4 A", "Ia", "IA", "I b", "I B", "Ib1", "IB1"};
+            var parser = new Infrastructure.ClassesParser.ClassesParser();
+
+            var expectedClasses = new[]
+            {
+                new Class(1, "a"),
+                new Class(2, "a"),
+                new Class(3, "a"),
+                new Class(4, "a"),
+                new Class(1, "a"),
+                new Class(1, "a"),
+                new Class(1, "b"),
+                new Class(1, "b"),
+                new Class(1, "b1"),
+                new Class(1, "b1")
+            };
+
+            var classes = await parser.Parse(classNames);
+
+            classes.Should().BeEquivalentTo(expectedClasses);
+        }
+
+        [Fact]
+        public async void CanParseEveryDigitYear()
+        {
+            var classNames = new[] {"1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a"};
+            var parser = new Infrastructure.ClassesParser.ClassesParser();
+
+            var expectedClasses = new[]
+            {
+                new Class(1, "a"), 
+                new Class(2, "a"), 
+                new Class(3,"a"), 
+                new Class(4, "a"), 
+                new Class(5, "a"), 
+                new Class(6, "a"), 
+                new Class(7, "a"), 
+                new Class(8, "a"), 
+                new Class(9, "a"), 
+                new Class(10, "a"), 
+                new Class(11, "a"), 
+                new Class(12, "a")
+            };
+
+            var result = await parser.Parse(classNames);
+            
+            result.Should().BeEquivalentTo(expectedClasses);
+        }
+
+        [Fact]
+        public async void CanParseEveryRomanYear()
+        {
+            var classNames = new[] {"Ia", "IIa", "IIIa", "IVa", "Va", "VIa", "VIIa", "VIIIa", "IXa", "Xa", "XIa", "XIIa"};
+            var parser = new Infrastructure.ClassesParser.ClassesParser();
+            
+            var expectedClasses = new[]
+            {
+                new Class(1, "a"), 
+                new Class(2, "a"), 
+                new Class(3,"a"), 
+                new Class(4, "a"), 
+                new Class(5, "a"), 
+                new Class(6, "a"), 
+                new Class(7, "a"), 
+                new Class(8, "a"), 
+                new Class(9, "a"), 
+                new Class(10, "a"), 
+                new Class(11, "a"), 
+                new Class(12, "a")
+            };
+
+            var result = await parser.Parse(classNames);
+
+            result.Should().BeEquivalentTo(expectedClasses);
+        }
+    }
+}
