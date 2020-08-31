2e5d600495aad2fd9cb07e2b261777c68504b729
KanarekLife <kanareklife@gmail.com>
2020-08-31 12:32:52 +0200

    Cleanup in WebApp files.

--- src/eru.WebApp/Assets/login.js
+++
@@ -1,7 +0,0 @@
-﻿import './scss/common.scss'
-import './scss/login.scss'
-
-document.querySelector('#cancel-button').onclick = function () {
-    document.querySelector('#Model_Username').value = '';
-    document.querySelector('#Model_Password').value = '';
-}
