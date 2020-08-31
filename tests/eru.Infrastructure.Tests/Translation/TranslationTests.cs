95c37fe818db7f1ec6f060c8911d2cccd49edbff
Blazej Sak <bazik33@hotmail.com>
2020-09-01 00:23:18 +0200

    add Translator unit test

---
+++ tests/eru.Infrastructure.Tests/Translation/TranslationTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eru.Infrastructure.Translation;
+using FluentAssertions;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace eru.Infrastructure.Tests.Translation
+{
+    public class TranslationTests
+    {
+        [Fact]
+        public async void CanRetrieveTranslations()
+        {
+            var options = Options.Create(new LocalizationOptions());
+            var factory = new ResourceManagerStringLocalizerFactory(options, NullLoggerFactory.Instance);
+            var localizer = new StringLocalizer<TranslationTests>(factory);
+
+            var translator = new Translator<TranslationTests>(localizer);
+
+            var en = await translator.TranslateString("test-value", "en");
+            var pl = await translator.TranslateString("test-value", "pl");
+
+            en.Should().Be("A test value!");
+            pl.Should().Be("Testowa wartosc!");
+        }
+    }
+}
