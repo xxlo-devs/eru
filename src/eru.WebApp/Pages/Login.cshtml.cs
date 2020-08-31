3034b0b7d4aa581bf0628caf196e98f15358e9a0
KanarekLife <kanareklife@gmail.com>
2020-08-31 14:22:34 +0200

    Add Login Panel translations.

--- src/eru.WebApp/Pages/Login.cshtml.cs
+++ src/eru.WebApp/Pages/Login.cshtml.cs
@@ -3,6 +3,7 @@ using eru.Domain.Entity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
 
 namespace eru.WebApp.Pages
 {
@@ -15,26 +16,35 @@ namespace eru.WebApp.Pages
     public class Login : PageModel
     {
         private readonly SignInManager<User> _signInManager;
+        private readonly IStringLocalizer<Login> _localizer;
 
-        public Login(SignInManager<User> signInManager)
+        public Login(SignInManager<User> signInManager, IStringLocalizer<Login> localizer)
         {
             _signInManager = signInManager;
+            _localizer = localizer;
         }
 
         [BindProperty]
         public LoginModel Model { get; set; }
         
-        public async Task<RedirectResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync()
         {
-            await _signInManager.PasswordSignInAsync(Model.Username, Model.Password, false, true);
-            
-            if (HttpContext.Request.Query.TryGetValue("ReturnUrl", out var redirect))
+            var result = await _signInManager.PasswordSignInAsync(Model.Username, Model.Password, false, true);
+            if (result.Succeeded)
             {
-                if (redirect.Count > 0)
-                    return Redirect(redirect[0]);
-            }
+                if (HttpContext.Request.Query.TryGetValue("ReturnUrl", out var redirect))
+                {
+                    if (redirect.Count > 0)
+                        return Redirect(redirect[0]);
+                }
 
-            return RedirectPermanent("/admin");
+                return RedirectPermanent("/admin");   
+            }
+            else
+            {
+                ModelState.AddModelError("cannot-login", _localizer["cannot-log-in"]);
+                return Page();
+            }
         }
     }
 }
