ee1cbea9c366e8a9d80517f71a7adb3fe3aa6828
Blazej Sak <bazik33@hotmail.com>
2020-09-05 15:45:40 +0200

    fix GetLangSelectorTests.cs

--- tests/eru.Infrastructure.Tests/PlatformClients/FacebookMessenger/Selector/GetLangSelectorTests.cs
+++ tests/eru.Infrastructure.Tests/PlatformClients/FacebookMessenger/Selector/GetLangSelectorTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using eru.Infrastructure.PlatformClients.FacebookMessenger.Models.SendApi;
 using eru.Infrastructure.PlatformClients.FacebookMessenger.ReplyPayload;
 using FluentAssertions;
@@ -24,8 +25,8 @@ namespace eru.Infrastructure.Tests.PlatformClients.FacebookMessenger.Selector
 
             var expected = new[]
             {
-                new QuickReply("English", new Payload(PayloadType.Lang, "en").ToJson()),
-                new QuickReply("Polish", new Payload(PayloadType.Lang, "pl").ToJson()),
+                new QuickReply(new CultureInfo("en").DisplayName, new Payload(PayloadType.Lang, "en").ToJson()),
+                new QuickReply(new CultureInfo("pl").DisplayName, new Payload(PayloadType.Lang, "pl").ToJson()),
                 new QuickReply("Cancel", new Payload(PayloadType.Cancel).ToJson())
             };
 
@@ -44,16 +45,16 @@ namespace eru.Infrastructure.Tests.PlatformClients.FacebookMessenger.Selector
 
             var expected = new[]
             {
-                new QuickReply("Swedish", new Payload(PayloadType.Lang, "sv").ToJson()),
-                new QuickReply("Slovak", new Payload(PayloadType.Lang, "sk").ToJson()),
-                new QuickReply("Romanian", new Payload(PayloadType.Lang, "ro").ToJson()),
-                new QuickReply("Portuguese", new Payload(PayloadType.Lang, "pt").ToJson()),
-                new QuickReply("German", new Payload(PayloadType.Lang, "de").ToJson()),
-                new QuickReply("Norwegian Bokmål", new Payload(PayloadType.Lang, "nb").ToJson()),
-                new QuickReply("Dutch", new Payload(PayloadType.Lang, "nl").ToJson()),
-                new QuickReply("Latvian", new Payload(PayloadType.Lang, "lv").ToJson()),
-                new QuickReply("Lithuanian", new Payload(PayloadType.Lang, "lt").ToJson()),
-                new QuickReply("Irish", new Payload(PayloadType.Lang, "ga").ToJson()),
+                new QuickReply(new CultureInfo("sv").DisplayName, new Payload(PayloadType.Lang, "sv").ToJson()),
+                new QuickReply(new CultureInfo("sk").DisplayName, new Payload(PayloadType.Lang, "sk").ToJson()),
+                new QuickReply(new CultureInfo("ro").DisplayName, new Payload(PayloadType.Lang, "ro").ToJson()),
+                new QuickReply(new CultureInfo("pt").DisplayName, new Payload(PayloadType.Lang, "pt").ToJson()),
+                new QuickReply(new CultureInfo("de").DisplayName, new Payload(PayloadType.Lang, "de").ToJson()),
+                new QuickReply(new CultureInfo("nb").DisplayName, new Payload(PayloadType.Lang, "nb").ToJson()),
+                new QuickReply(new CultureInfo("nl").DisplayName, new Payload(PayloadType.Lang, "nl").ToJson()),
+                new QuickReply(new CultureInfo("lv").DisplayName, new Payload(PayloadType.Lang, "lv").ToJson()),
+                new QuickReply(new CultureInfo("lt").DisplayName, new Payload(PayloadType.Lang, "lt").ToJson()),
+                new QuickReply(new CultureInfo("ga").DisplayName, new Payload(PayloadType.Lang, "ga").ToJson()),
                 new QuickReply("->", new Payload(PayloadType.Lang, 1).ToJson()),
                 new QuickReply("Cancel", new Payload(PayloadType.Cancel).ToJson()),
             };
@@ -73,7 +74,7 @@ namespace eru.Infrastructure.Tests.PlatformClients.FacebookMessenger.Selector
 
             var expected = new[]
             {
-                new QuickReply("English", new Payload(PayloadType.Lang, "en").ToJson()),
+                new QuickReply(new CultureInfo("en").DisplayName, new Payload(PayloadType.Lang, "en").ToJson()),
                 new QuickReply("<-", new Payload(PayloadType.Lang, 1).ToJson()),
                 new QuickReply("Cancel", new Payload(PayloadType.Cancel).ToJson()) 
             };
@@ -93,16 +94,16 @@ namespace eru.Infrastructure.Tests.PlatformClients.FacebookMessenger.Selector
 
             var expected = new[]
             {
-                new QuickReply("Spanish", new Payload(PayloadType.Lang, "es").ToJson()),
-                new QuickReply("Greek", new Payload(PayloadType.Lang, "el").ToJson()),
-                new QuickReply("French", new Payload(PayloadType.Lang, "fr").ToJson()),
-                new QuickReply("Finnish", new Payload(PayloadType.Lang, "fi").ToJson()),
-                new QuickReply("Estonian", new Payload(PayloadType.Lang, "et").ToJson()),
-                new QuickReply("Danish", new Payload(PayloadType.Lang, "da").ToJson()),
-                new QuickReply("Czech", new Payload(PayloadType.Lang, "cs").ToJson()),
-                new QuickReply("Croatian", new Payload(PayloadType.Lang, "hr").ToJson()),
-                new QuickReply("Bulgarian", new Payload(PayloadType.Lang, "bg").ToJson()),
-                new QuickReply("Polish", new Payload(PayloadType.Lang, "pl").ToJson()),
+                new QuickReply(new CultureInfo("es").DisplayName, new Payload(PayloadType.Lang, "es").ToJson()),
+                new QuickReply(new CultureInfo("el").DisplayName, new Payload(PayloadType.Lang, "el").ToJson()),
+                new QuickReply(new CultureInfo("fr").DisplayName, new Payload(PayloadType.Lang, "fr").ToJson()),
+                new QuickReply(new CultureInfo("fi").DisplayName, new Payload(PayloadType.Lang, "fi").ToJson()),
+                new QuickReply(new CultureInfo("et").DisplayName, new Payload(PayloadType.Lang, "et").ToJson()),
+                new QuickReply(new CultureInfo("da").DisplayName, new Payload(PayloadType.Lang, "da").ToJson()),
+                new QuickReply(new CultureInfo("cs").DisplayName, new Payload(PayloadType.Lang, "cs").ToJson()),
+                new QuickReply(new CultureInfo("hr").DisplayName, new Payload(PayloadType.Lang, "hr").ToJson()),
+                new QuickReply(new CultureInfo("bg").DisplayName, new Payload(PayloadType.Lang, "bg").ToJson()),
+                new QuickReply(new CultureInfo("pl").DisplayName, new Payload(PayloadType.Lang, "pl").ToJson()),
                 new QuickReply("<-", new Payload(PayloadType.Lang, 0).ToJson()), 
                 new QuickReply("->", new Payload(PayloadType.Lang, 2).ToJson()),
                 new QuickReply("Cancel", new Payload(PayloadType.Cancel).ToJson()),
